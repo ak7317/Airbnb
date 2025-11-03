7c6f1d93a89daf66977b9eb767e6c2ec2b74714b
ak7317 <aamankhan31636@gmail.com>
2025-11-03 23:33:07 +0530

    create login route and page

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const { route } = require("./listing");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup",(req,res)=> {
     res.render("users/signup.ejs");
@@ -20,10 +21,20 @@ router.post("/signup", wrapAsync(async(req,res) => {
   catch(e) {
     req.flash("error",e.message);
     res.redirect("/signup");
-  }
-   
-  
-    
+  }  
 }));
+router.get("/login",(req,res)=> {
+  res.render("users/login.ejs");
+});
+
+router.post("/login",
+   passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true
+  }),
+  async(req,res) => {
+    req.flash("success","Welcome back to Wanderlust!");
+    res.redirect("/listings");
+  });
 
 module.exports = router;
